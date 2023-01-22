8c6bed0c3c2bb82016a9e90151bc3bc93bc3bf88
vishavant <officialvishavant@gmail.com>
2023-01-23 00:13:16 +0530

    Profile Model CReated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    pic = models.ImageField(blank=True, upload_to='user_profile_pic')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
